adedf07e63e9931e8c187c25d117a5bd0f635dd4
Entity 1011 <goffingslander@gmail.com>
2025-08-19 17:34:28 +0200

    Initial version of the Nix Flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Cat Plymouth Theme";
+
+  inputs =
+  {
+    nixpkgs =
+    {
+        url = "github:nixos/nixpkgs/nixos-25.05";
+    };
+  };
+
+  outputs = inputs:
+  {
+    packages.x86_64-linux.default =
+    with import inputs.nixpkgs { system = "x86_64-linux"; };
+    stdenv.mkDerivation {
+      pname = "plymouththeme-cat";
+      version = "0.0.0";
+      src = ./PlymouthTheme-Cat;
+      dontBuild = true;
+      installPhase =
+      ''
+        mkdir -p $out/share/plymouth/themes/plymouththeme-cat
+        cp * $out/share/plymouth/themes/plymouththeme-cat
+        find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
+      '';
+    };
+  };
+}
